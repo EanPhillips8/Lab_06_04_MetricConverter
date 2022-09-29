5ffe664aab04167faa0700302bb737e5fae3b1d7
EanPhillips8 <eanphillips8@gmail.com>
2022-09-29 15:18:44 -0400

    oupoj

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+        String Trash = "";
+
+        double meter = 0;
+
+        System.out.print("\n Enter the length in meters: ");
+        if(in.hasNextDouble()) {
+           meter  = in.nextDouble();
+        }
+        else {
+            Trash = in.nextLine();
+            System.out.println("Error, please enter a number: ");
+        }
+
+        System.out.println("Inches: " + (meter* 39.37));
+        System.out.println("Feet: " + (meter * 3.28));
+        System.out.println("Miles: " + (meter * 0.000621371));
+
+
     }
 }
